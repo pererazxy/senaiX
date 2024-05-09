fa97cc09ba5a7061cf3693ae4e1ed99039fc0dee
pererazxy <ph287807@gmail.com>
2024-05-09 10:59:50 -0300

    att projeto senaiX

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import AppRoutes from "./app.routes";
+import AuthRoutes from "./auth.routes";
+
+const Routes = () => {
+    const { user } = useAuth();
+    return user ? <AppRoutes/> : <AuthRoutes/>;
+};
+
+export default Routes;
